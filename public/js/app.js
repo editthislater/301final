264395182dc134b8c592725af22e3c1b0f2d2512
Sue Duclos <sue.duclos@gmail.com>
2020-02-20 21:29:40 -0800

    added background carousel

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let slides = ['/assets/maps.jpeg', '/assets/pins.jpeg', '/assets/vintageglobe.jpeg', '/assets/globe-background.jpeg'];
+let imageIndex = 0;
+
+setInterval(() => {
+  let background = $('.idxbody');
+  background.animate({opacity: 1}, 250);
+  background.css('background-image', `url('${slides[imageIndex]}')`);
+  setTimeout(() => {
+    background.animate({opacity: 0.95}, 250);
+  }, 2750);
+  imageIndex++;
+  if (imageIndex === slides.length) {imageIndex = 0;}
+}, 3000);
+
