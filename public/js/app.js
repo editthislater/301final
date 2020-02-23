53142f7d1f9c145bbc6ab32b02dc86854c2e8dbb
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2020-02-22 16:32:50 -0800

    used jquery to filter languages

--- public/js/app.js
+++ public/js/app.js
@@ -18,31 +18,47 @@
 //show hide drop-downs based on region selection
 $(document).ready(function() {
   $('select.subregion').hide();
+  $('select.language').hide();
   $('select#region').on('change', function() {
     if ($(this).val() === 'africa') {
       $('select.subregion').hide();
       $('select#subregion-dummy').hide();
+      $('select.language').hide();
+      $('select#language-dummy').hide();
       $('select#subregion-africa').show();
+      $('select#language-africa').show();
     }
     if ($(this).val() === 'americas') {
       $('select.subregion').hide();
       $('select#subregion-dummy').hide();
+      $('select.language').hide();
+      $('select#language-dummy').hide();
       $('select#subregion-americas').show();
+      $('select#language-americas').show();
     }
     if ($(this).val() === 'asia') {
       $('select.subregion').hide();
       $('select#subregion-dummy').hide();
+      $('select.language').hide();
+      $('select#language-dummy').hide();
       $('select#subregion-asia').show();
+      $('select#language-asia').show();
     }
     if ($(this).val() === 'europe') {
       $('select.subregion').hide();
       $('select#subregion-dummy').hide();
+      $('select.language').hide();
+      $('select#language-dummy').hide();
       $('select#subregion-europe').show();
+      $('select#language-europe').show();
     }
     if ($(this).val() === 'oceania') {
       $('select.subregion').hide();
       $('select#subregion-dummy').hide();
+      $('select.language').hide();
+      $('select#language-dummy').hide();
       $('select#subregion-oceania').show();
+      $('select#language-oceania').show();
     }
   });
 
